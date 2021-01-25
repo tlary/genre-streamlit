0736810b951497a79a6070a4799e7176473efb82
Tobias Larysch <tobias-larysch@gmx.net>
2021-01-25 16:13:46 +0100

    add files to repo

---
+++ streamlitApp.py
@@ -0,0 +1,30 @@
+import streamlit as st
+import pickle
+from fastai.text.all import *
+import sentencepiece
+
+st.title("Classify your (German) lyrics! :tada::microphone::notes:")
+
+learn_inf = load_learner("genreModel.pkl")
+images = "./static/"
+
+expander = st.beta_expander("Lyrics")
+with expander:
+    lyrics = st.text_area("Paste your lyrics here:", height=100)
+    clicked = st.button("Classify lyrics!")
+
+if clicked:
+    st.spinner("Predicting the song's genre...")
+with st.beta_container():
+    # make prediction and edit output format
+    if clicked:
+        with st.spinner("Predicting the song's genre..."):
+            pred = learn_inf.predict(lyrics)[0]
+            img = images + pred + ".jpg"
+            # edit string output
+            if pred == "hiphop":
+                pred = "Hip-Hop"
+            if pred in ["pop", "schlager"]:
+                pred = pred.capitalize()
+            st.image(img, use_column_width=True)
+            st.info("The song's genre is " + pred)
